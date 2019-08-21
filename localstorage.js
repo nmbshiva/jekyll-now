81f8c714ed5c073bb5228df927da3aadc054d85d
Numb Shiva <nmbshiva@users.noreply.github.com>
2019-08-21 14:18:54 +0930

    Create localstorage.js

---
+++ localstorage.js
@@ -0,0 +1,3 @@
+for(var i=0; i<localStorage.length; i++) {                                               
+        console.log(localStorage.key(i), " = ", localStorage.getItem(localStorage.key(i)));
+}
